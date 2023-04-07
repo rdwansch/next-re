5f61a209866383a00facea21584053cd35852ec5
ujklm23 <ridwanrasyid345@gmail.com>
2023-04-07 17:36:57 +0700

    improve: tailwindcss performance on hot reload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    '!./node_modules',
+  ],
+};
