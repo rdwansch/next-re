f01bad9d8a9c9e7d6b30ad9803f44e06a7c76573
ujklm23 <ridwanrasyid345@gmail.com>
2023-04-08 20:55:08 +0700

    setting up database with prisma and supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+
+let counter = 0;
+
 export default function Home() {
+  const [name, setName] = useState('');
+
+  const submit = e => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <h1>Home Page</h1>
+      <form onSubmit={submit}>
+        <input type="text" onChange={e => setName(e.target.value)} />
+      </form>
+
+      {counter++}
     </>
   );
 }
