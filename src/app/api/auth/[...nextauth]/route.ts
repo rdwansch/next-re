304e4769b637727d3b394d52e191c8f247525776
ujklm23 <ridwanrasyid345@gmail.com>
2023-06-18 14:50:34 +0700

    add: CredentialsProviders

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,53 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
 import { prisma } from 'prisma/config';
+import bcrypt from 'bcrypt';
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  theme: {
+    colorScheme: 'light',
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'username', type: 'text', placeholder: 'enter username' },
+        password: { label: 'password', type: 'password', placeholder: 'enter password' },
+      },
+
+      async authorize(credentials, req) {
+        try {
+          const user = await prisma.user.findFirst({
+            where: {
+              username: `${credentials?.username}`,
+            },
+          });
+
+          if (user) {
+            const isValid = bcrypt.compareSync(credentials?.password!, user.password!);
+
+            if (!isValid) {
+              console.log('username or passowrd is wrong');
+              throw new Error('username or passowrd is wrong');
+            }
+            return user;
+          }
+          return null;
+        } catch (err) {
+          console.log('Error on Authorize:', err);
+          return null;
+        }
+      },
     }),
   ],
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import { signIn, useSession } from 'next-auth/react';
 export default function Home() {
   const session = useSession();
 
+  const handleSignin = async () => {
+    const res = await signIn('credentials', { username: 'test', password: 'test' });
+
+    console.log(res);
+  };
+
   return (
     <>
       {JSON.stringify(session)}
-      <button onClick={() => signIn('google')} className="text-xl px-10 py-2 bg-blue-600 rounded text-white mx-auto block">
+      <button onClick={() => handleSignin()} className="text-xl px-10 py-2 bg-blue-600 rounded text-white mx-auto block">
         Sign in
       </button>
     </>
