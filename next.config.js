2a85228ec4f35e66767f110381f2659813971cd9
ujklm23 <ridwanrasyid345@gmail.com>
2023-07-08 20:19:11 +0700

    add: swr and next-images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
   headers:
     process.env.NODE_ENV === 'development'
       ? () => [
